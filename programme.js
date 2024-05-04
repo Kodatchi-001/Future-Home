18f5a0153fff6f20720d185ddbd94953567e7385
Ahmed Hariri <150302563+Kodatchi-001@users.noreply.github.com>
2024-05-04 19:36:43 +0100

    create js in page-2 card

--- programme.js
+++ programme.js
@@ -7,7 +7,7 @@ function Characters() {
     // Diviser chaque caractère
     const chars = text.split('');
     // Placer chaque caractère dans un élément <span> séparé
-    const wrappedChars = chars.map((char,index) => `<span id="mov-${index}" style="animation:move ${index * 0.4}s;">${char}</span>`);
+    const wrappedChars = chars.map((char, index) => `<span id="mov-${index}" style="animation:move ${index * 0.4}s;">${char}</span>`);
     // Remplacer le contenu de l'élément de titre par les caractères enveloppés
     tittle.innerHTML = wrappedChars.join('');
 }
@@ -17,27 +17,36 @@ const tittle_2 = document.getElementById('tittle-home-2');
 function Characters_2() {
     const text = tittle_2.innerText;
     const chars = text.split('');
-    const wrappedChars = chars.map((char,index) => `<span id="mov-${index}" style="animation:move ${index * 0.4}s;">${char}</span>`);
+    const wrappedChars = chars.map((char, index) => `<span id="mov-${index}" style="animation:move ${index * 0.4}s;">${char}</span>`);
     tittle_2.innerHTML = wrappedChars.join('');
 }
 Characters_2();
 
 //cards-page-2
-function cards_content(){
+function cards_content() {
     const all_cards = document.querySelectorAll('.cards-info-page-2 a');
-    for( i = 0 ; i < all_cards.length ; i++){
-        all_cards[i].addEventListener('click', function() {
+    for (i = 0; i < all_cards.length; i++) {
+        all_cards[i].addEventListener('click', function () {
             var show_content = this.parentNode;
-            this.style.color = 'black'
-            this.style.fontWeight = '900'
-            this.style.height = '9vh'
-
-            show_content.style.height = '30%'
-            show_content.style.alignItems = 'start';
-
             const icon = this.querySelector('.bx-right-arrow-alt');
-            icon.style.transform = 'rotate(90deg)';
-            icon.style.opacity = '1'          
+            if (show_content.style.height == '25%') {
+                this.style.color = ''
+                this.style.fontWeight = ''
+                this.style.height = ''
+                show_content.style.height = ''
+                show_content.style.alignItems = '';
+                icon.style.transform = '';
+                icon.style.opacity = ''
+            }
+            else {
+                this.style.color = 'black'
+                this.style.fontWeight = '900'
+                this.style.height = '7.5vh'
+                show_content.style.height = '25%'
+                show_content.style.alignItems = 'start';
+                icon.style.transform = 'rotate(90deg)';
+                icon.style.opacity = '1'
+            }
         });
     }
 }
