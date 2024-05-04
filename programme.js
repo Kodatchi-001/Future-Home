fb7135e37d01d9f0a98467783242c6661293728c
Ahmed Hariri <150302563+Kodatchi-001@users.noreply.github.com>
2024-05-04 17:05:53 +0100

    create page-2

--- programme.js
+++ programme.js
@@ -22,3 +22,23 @@ function Characters_2() {
 }
 Characters_2();
 
+//cards-page-2
+function cards_content(){
+    const all_cards = document.querySelectorAll('.cards-info-page-2 a');
+    for( i = 0 ; i < all_cards.length ; i++){
+        all_cards[i].addEventListener('click', function() {
+            var show_content = this.parentNode;
+            this.style.color = 'black'
+            this.style.fontWeight = '900'
+            this.style.height = '9vh'
+
+            show_content.style.height = '30%'
+            show_content.style.alignItems = 'start';
+
+            const icon = this.querySelector('.bx-right-arrow-alt');
+            icon.style.transform = 'rotate(90deg)';
+            icon.style.opacity = '1'          
+        });
+    }
+}
+cards_content()
